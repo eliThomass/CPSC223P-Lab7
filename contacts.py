0ccb7052e7015c3c11c933396aed0854632e48ca
Eli Thomas <ecthomas05@gmail.com>
2024-10-16 14:08:24 -0700

    added modify_contact, delete_contact

--- contacts.py
+++ contacts.py
@@ -12,6 +12,7 @@ class Contacts:
 			print("File not found.")
 		except:
 			print("Unexpected error.")
+			
 	def add_contact(self, id, first_name, last_name):
 		if id in self.contact_dict.keys():
 			return "error"
@@ -23,6 +24,31 @@ class Contacts:
 				json.dump(self.contact_dict,f)
 		except Exception as e:
 			print(f"Unexpected error: {e}")
-		return self.contact_dict[id]
+		return f'{id} : {self.contact_dict[id]}'
 		
-	
+	def modify_contact(self, id, first_name, last_name):
+		if id not in self.contact_dict.keys():
+			return "error"
+		self.contact_dict[id] = fullname
+		self.contact_dict[id] = fullname
+		self.contact_dict = dict(sorted(self.contact_dict.items(), key=lambda item: item[1]))
+		try:
+			with open(self.name, 'w') as f:
+				json.dump(self.contact_dict,f)
+		except Exception as e:
+			print(f"Unexpected error: {e}")
+		return f'{id} : {self.contact_dict[id]}'
+		
+	def delete_contact(self, id):
+		if id not in self.contact_dict.keys():
+			return "error"
+		return_name = self.contact_dict[id]
+		self.contact_dict.pop(id)
+		try:
+			with open(self.name, 'w') as f:
+				json.dump(self.contact_dict,f)
+		except Exception as e:
+			print(f"Unexpected error: {e}")
+		return f'{id} : {return_name}'
+		
+		
